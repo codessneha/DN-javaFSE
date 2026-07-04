3cd30bf6d42c90d68b2673c77210776282f8ed75
Sneha Pandey <coding.snehapandey@gmail.com>
2026-07-04 20:17:30 +0530

    feat: implement Hello World RESTful web service with Spring Boot

---
+++ Spring REST using Spring Boot/2. spring-rest-handson/hello-world-rest/src/main/java/com/cognizant/helloworldrest/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.cognizant.helloworldrest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // Logger object
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String sayHello() {
+
+        LOGGER.info("Start of sayHello()");
+
+        String message = "Hello World!!";
+
+        LOGGER.info("End of sayHello()");
+
+        return message;
+    }
+}
