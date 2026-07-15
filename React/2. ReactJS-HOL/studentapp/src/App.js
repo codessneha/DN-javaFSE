22e990a60c9978c8b8d2fde91f2db441de5e9faa
Sneha Pandey <coding.snehapandey@gmail.com>
2026-07-15 21:17:33 +0530

    feat: create student management portal using multiple React class components

---
+++ React/2. ReactJS-HOL/studentapp/src/App.js
@@ -0,0 +1,23 @@
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+
+    return (
+
+        <div>
+
+            <Home />
+
+            <About />
+
+            <Contact />
+
+        </div>
+
+    );
+
+}
+
+export default App;
