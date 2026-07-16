894a64e35b7692ee56778e42f3394661887320e0
Sneha Pandey <coding.snehapandey@gmail.com>
2026-07-16 16:57:12 +0530

    feat: create score calculator using React functional component and CSS styling

---
+++ React/3. ReactJS-HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,23 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+
+  return (
+
+    <CalculateScore
+
+      name="Sneha Pandey"
+
+      school="ABC Public School"
+
+      total={500}
+
+      goal={5}
+
+    />
+
+  );
+
+}
+
+export default App;
